fc98628b0dee0ecc9523c2d54b54c46651ca1189
Umar Rahi <125821878+umarrahi@users.noreply.github.com>
2025-02-25 16:56:06 +0500

    update

--- app/page.js
+++ app/page.js
@@ -1,20 +1,6 @@
-"use client"
+import Image from "next/image";
 
-import React, { useState } from "react";
-
-const page = () => {
-
-  const [title, setTitle ]=useState("");
-  const [desc, setDesc ]=useState("");
-
-  const submitHandler =(e)=>{
-     e.preventDefault();
-     console.log(title)
-     console.log(desc)
-     setTitle("")
-     setDesc("")
-  }
-
+export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         {/* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -112,6 +98,4 @@ const page = () => {
         </footer> */}
     </div>
   );
-};
-
-export default page;
+}
