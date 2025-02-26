c204f898de3fa06ebc259202f2870e98aaab787e
Umar Rahi <rahiaptech@gmail.com>
2025-02-26 08:37:26 +0500

    update

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-"use client"
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 
 const page = () => {
   const [title, setTitle] = useState("");
@@ -8,51 +8,64 @@ const page = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    setMainTask([...mainTask, {title, desc}]);
-    setTitle("")
-    setDesc("")
-    console.log(mainTask)
+    setMainTask([...mainTask, { title, desc }]);
+    setTitle("");
+    setDesc("");
+    console.log(mainTask);
+  };
 
-    mainTask.map(()=>{
-      
-    })
+  let renderTask = <h2>Task is not available</h2>;
 
-  }
-
-  const renderTask = <h2>Task is not available</h2>
-
-  
+  renderTask = mainTask.map((t, i) => {
+    return (
+      <li className="flex justify-between">
+        <h2>{t.title}</h2>
+        <h3>{t.desc}</h3>
+      </li>
+    );
+  });
 
   return (
     <>
       <div>
-        <h1 className='p-5 bg-slate-600 text-slate-50 text-3xl font-bold text-center'>To-Do List</h1>
-        <form onSubmit={submitHandler} className='flex justify-center gap-5 py-10'>
+        <h1 className="p-5 bg-slate-600 text-slate-50 text-3xl font-bold text-center">
+          To-Do List
+        </h1>
+        <form
+          onSubmit={submitHandler}
+          className="flex justify-center gap-5 py-10"
+        >
           <input
-            type='text'
-            name=''
+            type="text"
+            name=""
             value={title}
-            onChange={(e) => { setTitle(e.target.value) }}
-            placeholder='Enter title'
-            className='border-slate-400 border-2 rounded-lg py-2 px-3' />
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            placeholder="Enter title"
+            className="border-slate-400 border-2 rounded-lg py-2 px-3"
+          />
           <input
-            type='text'
-            name=''
+            type="text"
+            name=""
             value={desc}
-            onChange={(e) => { setDesc(e.target.value) }}
-            placeholder='Enter description'
-            className='border-slate-400 border-2 rounded-lg py-2 px-3' />
-          <button className='bg-slate-700 text-slate-50 py-2 px-4 font-semibold rounded-lg'>Add task</button>
+            onChange={(e) => {
+              setDesc(e.target.value);
+            }}
+            placeholder="Enter description"
+            className="border-slate-400 border-2 rounded-lg py-2 px-3"
+          />
+          <button className="bg-slate-700 text-slate-50 py-2 px-4 font-semibold rounded-lg">
+            Add task
+          </button>
         </form>
 
-        <div className='bg-slate-300 p-5 text-center'>
-          <ul>
-            {renderTask}
-          </ul>
+        <div className="bg-slate-300 p-5 text-center">
+          <ul>{renderTask}</ul>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
